e3db3f3ac82e82cbb4235feb76e38e165987fe33
AIM-KH-3-02 <81522112+AIM-KH-3-02@users.noreply.github.com>
2021-05-25 21:43:31 +0300

    Update game3.py

--- game3.py
+++ game3.py
@@ -1,10 +1,12 @@
 import turtle
 import winsound
+import time
+import math
 import random
+import time
 
 wn = turtle.Screen()
 wn.title("Catch game by @AIM1")
-wn.bgcolor("green")
 wn.bgpic("sky.gif")
 wn.setup(width=800, height=600)
 wn.register_shape("pygame_left_1.gif")
@@ -15,8 +17,75 @@ wn.register_shape("pygame_idle.gif")
 
 
 score = 0
-lives = 3
+lives = 5
 x = 50
+def game_menu():
+    #Створюємо простеньке меню для игры
+    menu1 = turtle.Turtle()
+    menu1.penup()
+    # wn.bgpic("sky2.gif")
+    menu1.speed(0)
+    menu1.hideturtle()
+    menu1.goto(0, 180)
+    menu1.write("Main Menu", align="center", font=("verdana", 30, "bold"))
+
+    menu2 = turtle.Turtle()
+    menu2.speed(0)
+    menu2.penup()
+    menu2.goto(0, 100)
+    wn.register_shape("game_start.gif")
+    menu2.shape("game_start.gif")
+
+    menu3 = turtle.Turtle()
+    menu3.speed(0)
+    menu3.penup()
+    menu3.goto(0, 20)
+    wn.register_shape("quit_game.gif")
+    menu3.shape("quit_game.gif")
+
+    def click1 (x, y):
+        if x > menu2.xcor() - 63 and x < menu2.xcor() + 63 and y > menu2.ycor() - 25 and y < menu2.ycor() +25:
+            menu1.clear()
+            menu2.goto(0, 500)
+            menu3.goto(0, 500)
+        if x > menu3.xcor() - 63 and x < menu3.xcor() + 63 and y > menu3.ycor() - 25 and y < menu3.ycor() +25:
+            menu1.clear()
+            menu2.goto(0, 500)
+            menu3.goto(0, 500)
+            menu4 = turtle.Turtle()
+            menu4.penup()
+            menu4.speed(0)
+            menu4.hideturtle()
+            menu4.goto(0, 180)
+            menu4.write("Are you sure?", align="center", font=("verdana", 30, "bold"))
+            menu9 = turtle.Turtle()
+            menu9.speed(0)
+            menu9.penup()
+            menu9.goto(0, 100)
+            wn.register_shape("yes.gif")
+            menu9.shape("yes.gif")
+
+            menu10 = turtle.Turtle()
+            menu10.speed(0)
+            menu10.penup()
+            menu10.goto(0, 20)
+            wn.register_shape("no.gif")
+            menu10.shape("no.gif")
+            def click3(x, y):
+                if x > menu9.xcor() - 63 and x < menu9.xcor() + 63 and y > menu9.ycor() - 25 and y < menu9.ycor() +25:
+                    wn.bye()
+                if x > menu10.xcor() - 63 and x < menu10.xcor() + 63 and y > menu10.ycor() - 25 and y < menu10.ycor() +25:
+                    menu4.clear()
+                    menu9.goto(0, 500)
+                    menu10.goto(0, 500)
+                    game_menu()
+        turtle.listen()
+        turtle.onscreenclick(click3, 1)
+
+    turtle.listen()
+    turtle.onscreenclick(click1, 1)
+
+
 #Добавляем игрока
 player = turtle.Turtle()
 player.speed(0)
@@ -72,17 +141,16 @@ def go_right():
 wn.listen()
 wn.onkey(go_left, "Left")
 wn.onkey(go_right, "Right")
-
-
-while True:
+game_play = True
+while game_play:
     wn.update()
     #Передвежение игрока
-    if player.direction == "left" and x > -360:
+    if player.direction == "left" and player.xcor() > -360:
         x = player.xcor()
         x -= 20
         player.setx(x)
 
-    if player.direction == "right" and x < 360:
+    if player.direction == "right" and player.xcor() < 360:
         x = player.xcor()
         x += 20
         player.setx(x)
@@ -132,6 +200,30 @@ while True:
             lives -= 1
             txt.clear()
             txt.write("Score: {}  Lives: {}".format(score, lives), align="center", font=font)
+            if lives == 0:
+                wn.clear()
+                game_play = False
+                wn.bgpic("sky2.gif")
+                menu7 = turtle.Turtle()
+                menu7.color("yellow")
+                menu7.penup()
+                menu7.speed(0)
+                menu7.hideturtle()
+                menu7.goto(0, 180)
+                menu7.write("Game Over", align="center", font=("verdana", 30, "bold"))
 
+                menu6 = turtle.Turtle()
+                menu6.speed(0)
+                menu6.penup()
+                menu6.goto(0, 100)
+                wn.register_shape("go_to_menu.gif")
+                menu6.shape("go_to_menu.gif")
+                def click2 (x, y):
+                    if x > menu6.xcor() - 63 and x < menu6.xcor() + 63 and y > menu6.ycor() - 25 and y < menu6.ycor() +25:
+                        menu7.clear()
+                        menu6.goto(0, 500)
+                        game_menu()
+                turtle.listen()
+                turtle.onscreenclick(click2, 1)
 
 wn.mainloop()
